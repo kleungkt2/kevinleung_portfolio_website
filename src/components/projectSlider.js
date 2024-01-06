ba3b720029d4d98e3092e97e2bb4d67094751da0
kleungkt2 <leungkt.kevin@gmail.com>
2024-01-06 17:25:43 +0800

    WIP website

--- src/components/projectSlider.js
+++ src/components/projectSlider.js
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
-import { ProjectData  } from './projectData';
-import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
-import {ReactComponent as Cursor} from './../assets/images/cursorDark.svg';
+import React, { useState } from "react";
+import { ProjectData } from "./projectData";
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
+import { ReactComponent as Cursor } from "./../assets/images/cursorDark.svg";
 
-const ProjectSlider = ({setBackground}) => {
+const ProjectSlider = ({ setBackground }) => {
   const [current, setCurrent] = useState(0);
   const length = ProjectData.length;
 
   const backgroundImage = (b) => {
-    setBackground(b)
-  }
+    setBackground(b);
+  };
 
   const nextSlide = () => {
     setCurrent(current === length - 1 ? 0 : current + 1);
@@ -24,32 +24,42 @@ const ProjectSlider = ({setBackground}) => {
   }
 
   return (
-    <section className='slider'>
-      <Cursor className='left-arrow' onClick={prevSlide} />
-      <Cursor className='right-arrow' onClick={nextSlide} />
+    <section className="slider">
+      <Cursor className="left-arrow" onClick={prevSlide} />
+      <Cursor className="right-arrow" onClick={nextSlide} />
       {ProjectData.map((slide, index) => {
         return (
           <div
-            className={index === current ? 'slide active' : 'slide'}
+            className={index === current ? "slide active" : "slide"}
             key={index}
             onChange={setBackground(current)}
-          > 
-              {/* <img src={slide.img} alt='project image' className='image' /> */}
-              {/* <h4>{slide.title}</h4>
+          >
+            {/* <img src={slide.img} alt='project image' className='image' /> */}
+            {/* <h4>{slide.title}</h4>
               <p>{slide.description}</p>
               <p>{slide.stack}</p>
               <a href={slide.link}>visit</a> */}
             {index === current && (
-                <>
-              <img src={slide.img} alt='project image' className='image' />
-              <div className='detail'>
+              <>
+                <img src={slide.img} alt="project image" className="image" />
+                <div className="detail">
                   <h4>{slide.title}</h4>
-                  <p className='desc'>{slide.description}</p>
+                  <p className="desc">{slide.description}</p>
                   <span>
-                  <p className='stack'>{slide.stack}</p>
-                  <a href={slide.link} target='_blank'>visit ↵</a>
+                    {Array.isArray(slide.stack) ? (
+                      slide.stack.map((stack_text, i) => (
+                        <p key={i} className="stack">
+                          {stack_text}
+                        </p>
+                      ))
+                    ) : (
+                      <p className="stack">{slide.stack}</p>
+                    )}
+                    <a href={slide.link} target="_blank">
+                      visit ↵
+                    </a>
                   </span>
-              </div>
+                </div>
               </>
             )}
           </div>
